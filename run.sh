916ef6cd1466d5d4e0b8322ac8d9d86a8b65019e
Anton Artamonov <ant.artamonov@gmail.com>
2016-04-05 19:33:02 +0600

    Now we are changing owner only on existing directories.

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@
 if [ -d /mount/gce-volume ]; then
   rm -rf /var/www/pydio-core/data && ln -s /mount/gce-volume/pydio /var/www/pydio-core/data
   rm -rf /var/lib/mysql && ln -s /mount/gce-volume/mysql/ /var/lib/mysql
+
+  chown -R www-data:www-data /mount/gce-volume/pydio/
+  chown -R mysql:mysql /mount/gce-volume/mysql/
 fi
 
 # reset permissions on log volumes
 chown -R www-data:www-data /var/www/pydio-core
-chown -R www-data:www-data /mount/gce-volume/pydio/
-chown -R mysql:mysql /mount/gce-volume/mysql/
-
 chmod -R 770 /var/www/pydio-core
 chmod 777  /var/www/pydio-core/data/files/
 chmod 777  /var/www/pydio-core/data/personal/
